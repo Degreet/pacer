788c99086030334ea8469bb1e31c99dd9b0762bb
Degreet <degreetpro@gmail.com>
2020-10-01 16:09:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+require('c4console')
+const { MongoClient, ObjectId } = require("mongodb")
+const { createServer } = require('http')
+const fs = require('fs'), fsp = fs.promises
+const bcrypt = require('bcrypt')
+const Cookies = require('cookies')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+const pass = process.env.KEY
+const server = createServer(requestHandler)
+const uri = `mongodb+srv://Node:${pass}@cluster0-ttfss.mongodb.net/war-codes-js?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+async function requestHandler(req, resp) {
+  const cookies = new Cookies(req, resp)
+
+  let { url } = req
+  if (url.startsWith('/api/')) {
+    url = url.slice(5)
+  } else {
+    let path = process.cwd() + '/public' + url.replace(/\/$/, '')
+
+    try {
+      const target = await fsp.stat(path).catch(_ => fsp.stat(path += '.html'))
+      if (target.isDirectory()) path += '/index.html'
+      const match = path.match(/\.(\w+)$/), ext = match ? match[1] : 'html'
+
+      if (path.endsWith("/public/index.html")) {
+        const [file] = await Promise.all([fsp.readFile(path)])
+        const html = buildPage("Pacer", file.toString())
+        resp.setHeader('Content-Type', 'text/html')
+        resp.end(html)
+      } else {
+        fs.createReadStream(path).pipe(resp)
+        resp.setHeader('Content-Type', {
+          html: 'text/html',
+          json: 'application/json',
+          css: 'text/css',
+          ico: 'image/x-icon',
+          jpg: 'image/jpeg',
+          png: 'image/png',
+          gif: 'image/gif',
+          svg: 'image/svg+xml',
+          js: 'application/javascript',
+        }[ext])
+      }
+    } catch {
+      resp.end('"... sorry, ' + url + ' is not available"')
+    }
+  }
+}
+
+function streamToString(stream) {
+  const chunks = []
+  return new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk))
+    stream.on('error', reject)
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+  })
+}
+
+function buildPage(title, body) {
+  return /*html*/`
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+      <link rel="stylesheet" href="/css/main.css">
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+    </head>
+    <body>
+      ${body}
+    </body>
+    </html>
+  `
+}
+
+function generateToken() {
+  let res = ''
+  const chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
+  for (let i = 0; i < 32; i++) res += chars[Math.floor(Math.random() * chars.length)]
+  return res
+}
+
+client.connect(err => {
+  if (err) console.log(err)
+
+  global.users = client.db("war-codes-js").collection("users")
+  global.articles = client.db("war-codes-js").collection("articles")
+  global.answers = client.db("war-codes-js").collection("answers")
+
+  server.listen(PORT, () => console.log('Server started at http://localhost:3000'))
+  setTimeout(() => client.close(), 1e9)
+})
