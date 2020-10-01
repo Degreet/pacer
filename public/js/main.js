b8e8805070ce164339bd04b2f6d8fedcad9e462c
Degreet <degreetpro@gmail.com>
2020-10-01 18:02:10 +0300

    updated header, loader

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,34 @@
-header.classList.add("show")
-header.ontransitionend = () => {
-  setTimeout(() => {
-    title.classList.add("show")
-    title.ontransitionend = () => {
-      title.classList.add("end")
-      title.ontransitionend = () => desc.classList.add("show")
-    }
+if (localStorage.skipAnimation) document.querySelector("*").forEach(el => el.style.transition = "0s")
+
+onkeydown = e => {
+  if (e.key == 's') {
+    localStorage.skipAnimation = true
+  }
+}
+
+onload = () => {
+  const allButtons = document.querySelectorAll("button")
+  allButtons.forEach(btn => {
+    const redirectAttr = btn.getAttribute("redirect")
+    if (redirectAttr) btn.addEventListener("click", () => location.href = redirectAttr)
   })
+
+  loader.classList.add("loaded")
+  setTimeout(() => {
+    header.classList.add("show")
+    header.ontransitionend = () => {
+      setTimeout(() => {
+        title.classList.add("show")
+        title.ontransitionend = () => {
+          title.classList.add("end")
+          title.ontransitionend = () => {
+            desc.classList.add("show")
+            desc.ontransitionend = () => {
+              btns.classList.add("show")
+            }
+          }
+        }
+      })
+    }
+  }, 1000)
 }
